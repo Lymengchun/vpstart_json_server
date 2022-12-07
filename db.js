ec60a8e6ca4bfdc4c6e1a26bdf2369fcb96cef1d
lymengchun <lymengchun@gmail.com>
2022-12-07 15:46:31 +0700

    add more json file

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
 const user = require("./data/user");
-const notification = require("./data/notification")
+const notification = require("./data/notification");
+const news_events = require("./data/news_events.json");
+const vpstart_app = require("./data/vpstart_app.json");
+const vpstart_product = require("./data/vpstart_product.json");
+const appointment_request = require("./data/appointment_request.json");
 
 module.exports = {
     user,
-    notification
+    notification,
+    news_events,
+    vpstart_app,
+    vpstart_product,
+    appointment_request
 }
