b64f75c30e60329a1eb89bc13b6c48d32492879f
lymengchun <lymengchun@gmail.com>
2023-01-02 14:31:14 +0700

    video url

--- db.js
+++ db.js
@@ -4,6 +4,8 @@ const news_events = require("./data/news_events");
 const vpstart_app = require("./data/vpstart_app");
 const vpstart_product = require("./data/vpstart_product");
 const appointment_request = require("./data/appointment_request");
+const youtube_video = require("./data/youtube_video.json");
+
 module.exports = {
     user,
     notification,
@@ -11,6 +13,5 @@ module.exports = {
     vpstart_app,
     vpstart_product,
     appointment_request,
-  
-
+    youtube_video
 }
